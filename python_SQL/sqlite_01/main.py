887fe16cfe08e57c7c3fb1792fcdb30f37f11915
Fabio Schapowal <fabiosw@me.com>
2023-11-12 22:31:00 +0000

    sqlite

--- python_SQL/sqlite_01/main.py
+++ python_SQL/sqlite_01/main.py
@@ -10,6 +10,17 @@ TABLE_NAME = "customers"
 connection = sqlite3.connect(DB_FILE)
 cursor = connection.cursor()
 
+# APAGAR todos os dados da tabela (sem WHERE)
+cursor.execute(
+    f"DELETE FROM {TABLE_NAME}"
+)
+
+# ZERAR os ids das Tabelas
+cursor.execute(
+    f"DELETE FROM sqlite_sequence WHERE name='{TABLE_NAME}'"
+)
+
+# Criar tabela
 cursor.execute(
     f"CREATE TABLE IF NOT EXISTS {TABLE_NAME}"
     "("
@@ -17,6 +28,16 @@ cursor.execute(
     "name TEXT,"
     "weight REAL"
     ")"
+)
+
+connection.commit()
+
+# Inserir dados
+
+cursor.execute(
+    f"INSERT INTO {TABLE_NAME} (id, name, weight)"
+    "VALUES"
+    "(NULL, 'Fabio', 9.9),(NULL, 'Giuliana', 6.9)"
 )
 
 connection.commit()
